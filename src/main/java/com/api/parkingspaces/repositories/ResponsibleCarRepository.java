37fd8c5eb60cccc89897edc959bbb826d1a9eef0
muriloabreu <murilo.lilo@hotmail.com>
2023-06-07 22:28:06 -0300

    Criação da Interface ResponsibleCarRepository

---
+++ src/main/java/com/api/parkingspaces/repositories/ResponsibleCarRepository.java
@@ -0,0 +1,10 @@
+package com.api.parkingspaces.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.parkingspaces.models.ResponsibleCarModel;
+
+public interface ResponsibleCarRepository extends JpaRepository<ResponsibleCarModel, Long>{
+	
+	boolean existsByCpf(String cpf);
+}
