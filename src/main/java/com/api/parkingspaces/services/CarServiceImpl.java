5f5d6f3dd8ab4db9a10dc81717e2275d9e3b5941
muriloabreu <murilo.lilo@hotmail.com>
2023-06-15 20:51:06 -0300

    Implementação de anotação @Service na classe /CarServiceImpl.java

--- src/main/java/com/api/parkingspaces/services/CarServiceImpl.java
+++ src/main/java/com/api/parkingspaces/services/CarServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.api.parkingspaces.models.CarModel;
 import com.api.parkingspaces.repositories.CarRepository;
 
 import jakarta.transaction.Transactional;
 
+@Service
 public class CarServiceImpl implements CarService{
 	
 	@Autowired
