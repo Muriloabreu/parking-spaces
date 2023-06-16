bd7638952e5f5598816b742e91a123a6bea78657
muriloabreu <murilo.lilo@hotmail.com>
2023-06-15 22:50:42 -0300

    Implementação do Endpoint POST para inserir a Data no campo Date

--- src/main/java/com/api/parkingspaces/controllers/ResponsibleCarController.java
+++ src/main/java/com/api/parkingspaces/controllers/ResponsibleCarController.java
@@ -27,7 +27,7 @@ import jakarta.validation.Valid;
 
 @Controller
 @CrossOrigin(originPatterns = "*", maxAge = 3600) /*Para permitir ser acessado de qualquer fonte*/
-@RequestMapping("/parking-spaces/responsible") /*Mapeamento a nível de classe*/
+@RequestMapping("/parking-spaces/responsibles") /*Mapeamento a nível de classe*/
 public class ResponsibleCarController {
 	
 	@Autowired
