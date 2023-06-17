3ebe52aec179d278e8660541631b2f5e5dfabff3
muriloabreu <murilo.lilo@hotmail.com>
2023-06-16 22:39:09 -0300

    Implementação de novo método customizado usando Query Native na interface ParkingSpotRepository

--- src/main/java/com/api/parkingspaces/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingspaces/repositories/ParkingSpotRepository.java
@@ -1,12 +1,19 @@
 package com.api.parkingspaces.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.api.parkingspaces.models.ParkingSpotModel;
+import com.api.parkingspaces.projections.ParkingSpotMinJoinProjections;
 
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, Long> {
 	
 	boolean existsByParkingSpotNumberA(String name);
 	boolean existsByParkingSpotNumberB(String name);
 	boolean existsByApartmentAndBlock(String apartment, String block);
+	
+	@Query(nativeQuery = true, value = "")
+	List<ParkingSpotMinJoinProjections> findByJoinParkingSpot(String block);
 }
