1542baaf8aa0b6e995947f9e99fa00720c3fb864
muriloabreu <murilo.lilo@hotmail.com>
2023-06-16 22:34:24 -0300

    Criação de Interface para consulta customizada usando package Projections

---
+++ src/main/java/com/api/parkingspaces/projections/ParkingSpotMinJoinProjections.java
@@ -0,0 +1,10 @@
+package com.api.parkingspaces.projections;
+
+public interface ParkingSpotMinJoinProjections {
+	
+	String getParkingSpotNumberA();
+	String getParkingSpotNumberB();
+	String getNameBlock();
+	String getNameApartment();
+	String getNameCars();
+}
