d2ff3f7f1b28f8a1ba2ac30ccb3631431193b9e5
muriloabreu <murilo.lilo@hotmail.com>
2023-06-17 20:32:17 -0300

    Correção de atributo, foi removido

--- src/main/java/com/api/parkingspaces/dto/ParkingSpotDto.java
+++ src/main/java/com/api/parkingspaces/dto/ParkingSpotDto.java
@@ -1,10 +1,10 @@
 package com.api.parkingspaces.dto;
 
 import java.util.List;
-import java.util.Objects;
+
 
 import com.api.parkingspaces.models.CarModel;
-import com.api.parkingspaces.models.ResponsibleCarModel;
+
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,27 +17,20 @@ public class ParkingSpotDto {
 	private String parkingSpotNumberB;
 	@NotNull
 	private List<CarModel> cars;
-	@NotBlank
-	private ResponsibleCarModel responsibleCar;
-	@NotBlank
-	private String apartment;
-	@NotBlank
-	private String block;
+	
 	
 	public ParkingSpotDto() {
 		
 	}
 	
 	public ParkingSpotDto(@NotBlank String parkingSpotNumberA, @NotBlank String parkingSpotNumberB,
-			@NotNull List<CarModel> cars, @NotBlank ResponsibleCarModel responsibleCar, @NotBlank String apartment,
-			@NotBlank String block) {
-		super();
+			@NotNull List<CarModel> cars) {
+		
 		this.parkingSpotNumberA = parkingSpotNumberA;
 		this.parkingSpotNumberB = parkingSpotNumberB;
 		this.cars = cars;
-		this.responsibleCar = responsibleCar;
-		this.apartment = apartment;
-		this.block = block;
+		
+		
 	}
 	
 	public String getParkingSpotNumberA() {
@@ -62,25 +55,7 @@ public class ParkingSpotDto {
 	public void setCars(List<CarModel> cars) {
 		this.cars = cars;
 	}
-	public ResponsibleCarModel getResponsibleCar() {
-		return responsibleCar;
-	}
-	public void setResponsibleCar(ResponsibleCarModel responsibleCar) {
-		this.responsibleCar = responsibleCar;
-	}
-	public String getApartment() {
-		return apartment;
-	}
-	public void setApartment(String apartment) {
-		this.apartment = apartment;
-	}
-	public String getBlock() {
-		return block;
-	}
-	public void setBlock(String block) {
-		this.block = block;
-	}
-
+	
 
 	
 	
