7707824beb821fc55c01d087808b7710e155d003
muriloabreu <murilo.lilo@hotmail.com>
2023-06-18 21:52:08 -0300

    Criação de método List<findByParkingSpotAndBlock> na interface CarService

--- src/main/java/com/api/parkingspaces/services/CarService.java
+++ src/main/java/com/api/parkingspaces/services/CarService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.api.parkingspaces.models.CarModel;
+import com.api.parkingspaces.projections.ParkingSpotAndBlockMinJoinProjections;
 
 public interface CarService {
 	
 	List<CarModel> findAll();
+	List<ParkingSpotAndBlockMinJoinProjections> findByParkingSpotAndBlock(String block);
 	Optional<CarModel> findById(Long id);
 	CarModel save(CarModel car);
 	void delete(CarModel car);
