23ee29a1f1d7bcfa17f2c582b9c3c48c9f09c570
muriloabreu <murilo.lilo@hotmail.com>
2023-06-18 21:50:30 -0300

    Implementação no método findByJoinCarsAndBlock na Query Native

--- src/main/java/com/api/parkingspaces/repositories/CarRepository.java
+++ src/main/java/com/api/parkingspaces/repositories/CarRepository.java
@@ -8,16 +8,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.api.parkingspaces.models.CarModel;
-import com.api.parkingspaces.projections.ParkingSpotMinJoinProjections;
+import com.api.parkingspaces.projections.ParkingSpotAndBlockMinJoinProjections;
 
 public interface CarRepository extends JpaRepository<CarModel, Long>{
 	
 	boolean existsByLicensePlateCar(String licensePlateCar);
 	
-	@Query(nativeQuery = true, value = "SELECT car.license_plate_car, car.model_car, res.first_name, res.last_name, res.cpf, par.parking_spot_numbera, par.parking_spot_numberb FROM tb_car car"
+	@Query(nativeQuery = true, value = "SELECT par.parking_spot_numbera AS parkingSpotNumberA, par.parking_spot_numberb AS parkingSpotNumberB, res.block AS nameBlock, res.apartment AS nameApartment , car.model_car AS ModelCar, car.license_plate_car AS licenseCar, res.first_name AS firstName, res.last_name AS lastName "
+			+ "FROM tb_car car "
 			+ "INNER JOIN tb_responsible_car res ON res.id = car.responsible_car_id "
 			+ "INNER JOIN tb_parkings_pot par ON par.id = car.parkingspot_id "
-			+ "WHERE res.block LIKE '%B%'")
-	List<ParkingSpotMinJoinProjections> findByJoinCarsAndBlock(String Block);
+			+ "WHERE res.block LIKE %?1% " )
+	List<ParkingSpotAndBlockMinJoinProjections> findByJoinCarsAndBlock(String Block);
 
 }
