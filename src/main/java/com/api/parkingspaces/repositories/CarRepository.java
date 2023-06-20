a120fa871924390eacef3af103a2fb6ae4323fb0
muriloabreu <murilo.lilo@hotmail.com>
2023-06-19 23:03:59 -0300

    Implementação no método @Query Native

--- src/main/java/com/api/parkingspaces/repositories/CarRepository.java
+++ src/main/java/com/api/parkingspaces/repositories/CarRepository.java
@@ -14,7 +14,10 @@ public interface CarRepository extends JpaRepository<CarModel, Long>{
 	
 	boolean existsByLicensePlateCar(String licensePlateCar);
 	
-	@Query(nativeQuery = true, value = "SELECT par.parking_spot_numbera AS parkingSpotNumberA, par.parking_spot_numberb AS parkingSpotNumberB, res.block AS nameBlock, res.apartment AS nameApartment , car.model_car AS ModelCar, car.license_plate_car AS licenseCar, res.first_name AS firstName, res.last_name AS lastName "
+	@Query(nativeQuery = true, value = "SELECT par.parking_spot_numbera AS parkingSpotNumberA, "
+			+ "par.parking_spot_numberb AS parkingSpotNumberB, "
+			+ "res.block AS nameBlock, res.apartment AS nameApartment , car.model_car AS ModelCar, "
+			+ "car.license_plate_car AS licenseCar, res.first_name AS firstName, res.last_name AS lastName "
 			+ "FROM tb_car car "
 			+ "INNER JOIN tb_responsible_car res ON res.id = car.responsible_car_id "
 			+ "INNER JOIN tb_parkings_pot par ON par.id = car.parkingspot_id "
