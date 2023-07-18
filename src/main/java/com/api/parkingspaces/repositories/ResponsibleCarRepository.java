a2c5acc635ae82f36002ed3dd7c3a9b49d4e190c
muriloabreu <murilo.lilo@hotmail.com>
2023-07-17 22:44:36 -0300

    Add de método buscar por nome na interface ResponsibleCarRepository, usando @Query

--- src/main/java/com/api/parkingspaces/repositories/ResponsibleCarRepository.java
+++ src/main/java/com/api/parkingspaces/repositories/ResponsibleCarRepository.java
@@ -1,6 +1,9 @@
 package com.api.parkingspaces.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.api.parkingspaces.models.ResponsibleCarModel;
 
@@ -8,5 +11,7 @@ public interface ResponsibleCarRepository extends JpaRepository<ResponsibleCarMo
 	
 	boolean existsByCpf(String cpf);
 	
+	@Query(value = "select r from ResponsibleCarModel r where r.ResponsibleCar like %?1%")
+	List<ResponsibleCarModel> findByResponsibles(String name);
 	
 }
